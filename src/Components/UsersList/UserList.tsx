e14559c88e83bc8b5c889c50a06373c17da1d090
Muza21 <lasha.muzashvili1@gmail.com>
2024-12-17 13:15:27 +0400

    feat: add sort by age

--- src/Components/UsersList/UserList.tsx
+++ src/Components/UsersList/UserList.tsx
@@ -1,67 +1,91 @@
-import React, { useState } from "react"
-import { data, Person } from "../../static/data"
-import UsersListTableHead, {SortType} from "../UsersListTableHead/UsersListTableHead"
-import styles from './UsersList.module.css'
-import UserListItem from "../UserItem/UserListItem"
+import React, { useState } from "react";
+import { data, Person } from "../../static/data";
+import UsersListTableHead, {
+  SortType,
+} from "../UsersListTableHead/UsersListTableHead";
+import styles from "./UsersList.module.css";
+import UserListItem from "../UserItem/UserListItem";
 
 const UserList = () => {
   const [users, setUsers] = useState<Person[]>(data);
   const [nameSort, setNameSort] = useState<SortType>();
+  const [ageSort, setAgeSort] = useState<SortType>();
 
   const removeUser = (id: number) => {
     setUsers((previousUsers) => {
-      return previousUsers.filter((user) => user.id !== id )
-    })
-  }
-
+      return previousUsers.filter((user) => user.id !== id);
+    });
+  };
 
   const sortByFirstName = () => {
-    if (nameSort === 'asc') {
+    if (nameSort === "asc") {
       setUsers((previousUsers) => {
         const newUsers = [...previousUsers];
         newUsers.sort((userA, userB) => {
-          if (userA.first_name > userB.first_name) return 1
-          if (userA.first_name < userB.first_name) return -1
-          return 0
-        })
-        return newUsers
-      })
-      setNameSort('desc')
+          if (userA.first_name > userB.first_name) return 1;
+          if (userA.first_name < userB.first_name) return -1;
+          return 0;
+        });
+        return newUsers;
+      });
+      setNameSort("desc");
     } else {
       setUsers((previousUsers) => {
         const newUsers = [...previousUsers];
         newUsers.sort((userA, userB) => {
-          if (userA.first_name > userB.first_name) return -1
-          if (userA.first_name < userB.first_name) return 1
-          return 0
-        })
-        return newUsers
-      })
-      setNameSort('asc')
+          if (userA.first_name > userB.first_name) return -1;
+          if (userA.first_name < userB.first_name) return 1;
+          return 0;
+        });
+        return newUsers;
+      });
+      setNameSort("asc");
     }
+  };
 
-  }
+  const sortByAge = () => {
+    if (ageSort === "asc") {
+      setUsers((previousUsers) => {
+        const newUsers = [...previousUsers];
+        newUsers.sort((userA, userB) => {
+          return userB.age - userA.age;
+        });
+        return newUsers;
+      });
+      setAgeSort("desc");
+    } else {
+      setUsers((previousUsers) => {
+        const newUsers = [...previousUsers];
+        newUsers.sort((userA, userB) => {
+          return userA.age - userB.age;
+        });
+        return newUsers;
+      });
+      setAgeSort("asc");
+    }
+  };
 
   return (
     <div className={styles.table_container}>
       <table>
-        <UsersListTableHead 
-          sortByFirstName={sortByFirstName} 
-          nameSort={nameSort} />
+        <UsersListTableHead
+          sortByFirstName={sortByFirstName}
+          nameSort={nameSort}
+          sortByAge={sortByAge}
+          ageSort={ageSort}
+        />
         <tbody>
           {users.map((user) => {
-            return <React.Fragment key={user.id}>
-              <UserListItem 
-                user={user} 
-                removeUser={removeUser}
-              />
-            </React.Fragment>
-            }
-          )}
+            return (
+              <React.Fragment key={user.id}>
+                <UserListItem user={user} removeUser={removeUser} />
+              </React.Fragment>
+            );
+          })}
         </tbody>
       </table>
     </div>
-  )
-}
+  );
+};
 
-export default UserList
+export default UserList;
